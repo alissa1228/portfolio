1eea02174023d720fee24a155e6afbf6618d1793
alissa1228 <68986081+alissa1228@users.noreply.github.com>
2020-11-12 19:59:05 +0900

    JS navbar menu click

--- main.js
+++ main.js
@@ -15,3 +15,24 @@ document.addEventListener('scroll',()=>{
         navbar.classList.remove('navbar-dark');
     }
 });
+
+
+// scoll to section(Handle scolling when tapping on the navbar menu)
+
+/*1. 클릭을 했을 때 원하는 아이디를 알아야함
+2. 눌렀을 때 좌표로 스크롤링
+*/
+const navbarmenu = document.querySelector('.navbar_menu');
+navbarmenu.addEventListener('click',(e)=>{
+    const target = e.target;
+    const link = target.dataset.link;
+
+    if(link==null) {
+        return;
+    }
+    console.log(e.target.dataset.link); //우리가 data-link로 정의한 요소가 클릭하면 나온다
+    const scollTo = document.querySelector(link);
+    scollTo.scrollIntoView({behavior : "smooth"});
+});
+
+
